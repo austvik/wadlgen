a44e1cef0c177da0a257679e44d222c49fb32e52
Dan Knox <dknox@threedotloft.com>
2011-12-03 16:45:34 -0800

    Fixed regex typo.

--- lib/wadlgen/route_parser.rb
+++ lib/wadlgen/route_parser.rb
@@ -26,7 +26,7 @@ module Wadlgen
         controller = defaults[:controller]
         action = defaults[:action]
 
-        next if /\//
+        next if controller.match /\//
         next if action == 'edit'
 
         resource = ress.get_resource(nil, route.path)
